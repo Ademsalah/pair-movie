ea88c5aed5da4cb612d588800be29458f25601ab
brushurteeth8 <adem.hameed.123@gmail.com>
2024-03-12 19:17:34 +0100

    side bar wmezelt adding only with refresh

--- main.js
+++ main.js
@@ -1,39 +1,78 @@
-$("button").click(function () {
-  $(".movie").remove();
-});
 function generateId(n) {
   return Math.floor(Math.random() * (n + 1));
 }
+/* var value = 0;
+function generate() {
+  value += 1;
+
+  return value;
+} */
+
 var moviesList = JSON.parse(localStorage.getItem("movie")) || [];
-$(".submit").click(function () {
-  var name = $("#name").val();
-  var img = $("#URL").val();
-  var description = $("#des").val();
+$(".add").click(function () {
+  var name = $("#inp1").val();
+  var img = $("#inp3").val();
+  var description = $("#inp2").val();
   var movie = {
     name: "",
     Image: "",
     des: "",
   };
-
   movie.name = name;
-
   movie.Image = img;
   movie.des = description;
-  movie.id = generateId(8);
+  movie.id = generateId(4);
+
   moviesList.push(movie);
   localStorage.setItem("movie", JSON.stringify(moviesList));
-
-  //   let movie = localStorage.getItem("name");
-  //   let photo = localStorage.getItem("img");
-  //   let dess = localStorage.getItem("description");
-  //   console.log(movie);
-  //   $(".container").append(`
-  //         <div class="movie">
-  //           <h3>${movie}</h3>
-  //           <img src="${photo}" alt="">
-  //           <p>${dess}</p>
-  //         </div>`);
 });
+let movie = JSON.parse(localStorage.getItem("movie"));
+console.log(movie);
 
+var film = movie.map(
+  (movie) =>
+    ` <div class="movie" >
+            <img src="${movie.Image}" alt="" />
+            <h3>${movie.name}</h3>
+          <p>${movie.des}</p>
+         <a href="formulaire.html"> <i class="fa fa-edit"></i></a>
+          <i class="fa fa-remove"></i>
+        </div>`
+);
 
+console.log(film);
+$(".container").append(film);
 console.log(localStorage);
+
+$("#btn1").click(function () {
+  $("#movie1").remove();
+});
+$("#btn2").click(function () {
+  $("#movie2").remove();
+});
+$("#btn3").click(function () {
+  $("#movie3").remove();
+});
+$("#btn4").click(function () {
+  $("#movie4").remove();
+});
+$("#btn5").click(function () {
+  $("#movie5").remove();
+});
+$("#btn6").click(function () {
+  $("#movie6").remove();
+});
+$("#btn7").click(function () {
+  $("#movie7").remove();
+});
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = "none";
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
